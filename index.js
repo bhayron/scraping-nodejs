19dc8e72d9068cf3a8341fe1f47d992a135ce0ef
Bhayron <bhayronklivilan@gmail.com>
2023-09-17 21:14:33 -0300

    up

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const cheerio = require('cheerio');
 const URL = 'https://www.imdb.com/title/tt0102926/?ref_=nv_sr_1';
 
 (async () => {
-    
+
     const response = await request(URL);
 
     let $ = cheerio.load(response);
-    
-    let title = $('div[class="sc-dffc6c81-0"] > h1 > span').text();
+
+    let title = $('div[class="sc-dffc6c81-0 iwmAVw"] > h1').text();
     let rating = $('span[itemprop="ratingValue"]').text();
 
-    console.log("dadis",title, rating);
+    console.log("dadis", title, rating);
 })()
