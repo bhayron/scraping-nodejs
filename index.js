130967a4ad92e283a66d48ff61486c3d8e3a3cda
Bhayron <bhayronklivilan@gmail.com>
2023-09-17 21:34:02 -0300

    up

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const URL = 'https://www.imdb.com/title/tt0102926/?ref_=nv_sr_1';
     let $ = cheerio.load(response);
 
     let title = $('div[class="sc-dffc6c81-0 iwmAVw"] > h1').text();
-    let rating = $('span[class="sc-bde20123-1 iZlgcd"]').text();
+    let rating1 = $('span[class="sc-bde20123-1 iZlgcd"]').text();
 
-    console.log("dadis", title, rating);
+    const ratingDiv = $('div[data-testid="hero-rating-bar__aggregate-rating__score"]');
+    const secondSpan = ratingDiv.find('span').eq(1); // Pega o segundo span dentro da div
+
+    const rating2 = secondSpan.text();
+
+
+    console.log("dadis", title, rating1 + rating2);
 })()
